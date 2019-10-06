1e8b8312f177226b8cf0fa07f468aa8c3a52e7c8
rrwiyatn <rey.reza@hotmail.com>
2019-10-05 22:28:48 -0400

    fin ex3

--- notebooks/code/exercise_03_control/controller.py
+++ notebooks/code/exercise_03_control/controller.py
@@ -1,11 +1,12 @@
 import numpy as np
-import time
 
 class Controller():
     def __init__(self):
         self.gain = 2.0
-        # self.last_error_dist = None
-        self.integral_term = 0.0
+        self.last_dist = None
+        self.last_angle = None
+        self.integral_dist = 0.0
+        self.integral_angle = 0.0
         pass
 
     def angle_control_commands(self, dist, angle):
@@ -25,7 +26,7 @@ class Controller():
         # YOUR CODE HERE
         #
         v = 0.5
-        k_theta = 10.0
+        k_theta = 20.0
         theta_threshold = np.pi / 6.
         kd = -(k_theta**2) / (4.*v)
         d_threshold = abs((k_theta * theta_threshold) / kd)
@@ -43,41 +44,40 @@ class Controller():
         
         return  omega
 
-
-    def pid_angle_control_commands(self, dist, angle):
+    
+    def pid_control_commands(self, dist, angle, kp_dist, kp_angle, kd_dist, kd_angle, ki_dist, ki_angle):
         # Return the angular velocity in order to control the Duckiebot so that it follows the lane.
         # Parameters:
         #     dist: distance from the center of the lane. Left is negative, right is positive.
         #     angle: angle from the lane direction, in rad. Left is negative, right is positive.
+        #     kp_dist: constant for proportial term with distance error
+        #     kp_angle: constant for proportial term with angle error
+        #     kd_dist: constant for derivative term with distance error
+        #     kd_angle: constant for derivative term with angle error
+        #     ki_dist: constant for integral term with distance error
+        #     ki_angle: constant for integral term with angle error
         # Outputs:
         #     omega: angular velocity, in rad/sec. Right is negative, left is positive.
-        current_time = time.time()
-        v = 0.5
-        k_theta = 10.0
-        ki = 0.5
-        theta_threshold = np.pi / 6.
-        kd = -(k_theta**2) / (4.*v)
-        d_threshold = abs((k_theta * theta_threshold) / kd)
-        if dist < -d_threshold:
-            sat = -d_threshold
-        elif dist > d_threshold:
-            sat = d_threshold
+        if self.last_dist is None: # If first reading, use only P-term
+            omega = (kp_dist * dist) + (kp_angle * angle)
         else:
-            sat = dist
-        error_dist = -sat
-        error_angle = angle
-        # self.last_error_dist = error_dist
-        self.integral_term += sat
-        omega = (kd * error_dist) + (k_theta * error_angle) + (ki * self.integral_term)
-        # omega = (kd * error_dist) + (ki * self.integral_term)
+            diff_dist = dist - self.last_dist
+            diff_angle = angle - self.last_angle
+            omega = (kp_dist * dist) + (kp_angle * angle) + (kd_dist * diff_dist) + (kd_angle * diff_angle) + (ki_dist * self.integral_dist) + (ki_angle * self.integral_angle)
+        self.integral_dist += dist
+        self.integral_angle += angle
+        self.last_dist = dist
+        self.last_angle = angle
         return omega
 
+
     def wrap_angle(self, angle):
-        new_angle = angle % (np.pi * 2)
-        if new_angle < 0:
-            new_angle = new_angle + (2 * np.pi)
-        elif new_angle >= np.pi:
-            new_angle = new_angle - (2 * np.pi)
+        # Return the angle such that it is in the range [0,+2pi]
+        # Parameters:
+        #     angle: angle in rad
+        # Outputs:
+        #     new_angle: angle wrapped in the range [0,+2pi]
+        new_angle = angle % (2 * np.pi)
         return new_angle
     
 
@@ -121,24 +121,20 @@ class Controller():
             lookup_distance *= multiplier
         ########
         #
-        #TODO 2: Modify omega
+        # TODO 2: Modify omega
         #
         ######## 
-        # angle = -angle
-        duck_to_point = curve_point - pos # (x,y,z)
-        dist = np.linalg.norm(duck_to_point) # a scalar
-        unit_duck_to_point = duck_to_point / dist # (x,y,z)
-        z_comp = duck_to_point[2]
-        x_comp = duck_to_point[0]
-        angle_between_x_axis_and_target = np.arctan2(-z_comp,x_comp)
+        duck_to_point = curve_point - pos
+        dist = np.linalg.norm(duck_to_point)
+        unit_duck_to_point = duck_to_point / dist
+        angle_between_x_axis_and_target = np.arctan2(-duck_to_point[2], duck_to_point[0])
         alpha = angle - angle_between_x_axis_and_target
-
-        #alpha = np.arccos(np.dot(dir_unit_vector, unit_duck_to_point))
-        K = 0.2
-        omega = -(np.sin(alpha)) / (K) # Scaling dist with speed
+        K = 0.1 # Gain assuming L = K * V (making the lookup distance as a function of some gain times the velocity)
+        omega = -(np.sin(alpha)) / (K) # Since we write L = K * V, the equation for omega is simplified to this.
         v = 0.5
         
         crosstrack_error = np.sin(alpha) * dist # For plotting crosstrack error
-        angle_error = np.arctan2(closest_tangent[2],closest_tangent[0]) - self.wrap_angle(angle) # For plotting angle error in notebook
-
+        angle_error = self.wrap_angle(np.arctan2(-closest_tangent[2],closest_tangent[0])) - self.wrap_angle(angle) # For plotting angle error in notebook
+        if angle_error > np.pi:
+            angle_error = 2 * np.pi - angle_error
         return v, omega, crosstrack_error, angle_error
